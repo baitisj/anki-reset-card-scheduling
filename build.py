d61a2483fe69548445979a81cc973deb48bbd73f
Jeff 'Raid' Baitis <jeff@baitis.net>
2015-10-08 14:04:05 -0700

    Fix build script to ignore the build script :-)

--- build.py
+++ build.py
@@ -2,17 +2,17 @@
 import os
 import zipfile
 
-outname='reset-card-scheduling.zip'
+outname='voice-control.zip'
 
 def zipdir(path,ziph):
   # ziph is zipfile handle
   for root, dirs, files in os.walk(path):
     for file in files:
+      if 'build.py' in file:
+        continue
       if '.py' in file:
-        #ziph.write(os.path.join(root, file), os.path.relpath(os.path.join(root,file), os.path.join(path, '..')))
         ziph.write(os.path.join(root, file))
 
 if __name__ == '__main__':
   zipf = zipfile.ZipFile(outname, 'w')
   zipdir('.', zipf)
-  
